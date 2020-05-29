d8da2a3a28f7c422c3985c1710eae3a2e068ac24
Ivlev Aleksandr <a.y.ivlev@gmail.com>
2020-05-29 19:44:15 +0300

    CurrencyConv

--- homework-1/CurrencyConv/main.go
+++ homework-1/CurrencyConv/main.go
@@ -1,1 +1,25 @@
+package main
 
+import (
+	"fmt"
+	"log"
+	"strconv"
+)
+
+const dollar = 70.75
+const euro = 78.55
+
+func main() {
+	fmt.Printf("Курсы валют на сегодня $-%.2f руб за доллар и евро €-%.2f руб за евро.\n", dollar, euro)
+	fmt.Println("Какую сумму в рублях вы хотите обменять?")
+	summa := "10000"
+	fmt.Scanln(&summa)
+	rub, err := strconv.Atoi(summa)
+	if err != nil {
+		log.Fatalln(err)
+	}
+	fmt.Printf("Сумма в рублях %d\n", rub)
+	convdollar := float64(rub) / dollar
+	conveuro := float64(rub) / euro
+	fmt.Printf("После обмена вы получите %.2f$ или евро %.2f€.\n", convdollar, conveuro)
+}
