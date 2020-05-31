39e2b5836fd26c40f05b5f68feed06b24dfe9c17
Ivlev Aleksandr <a.y.ivlev@gmail.com>
2020-05-31 16:25:40 +0300

    Final realese 4

--- homework-1/AreaTriangle/main.go
+++ homework-1/AreaTriangle/main.go
@@ -15,15 +15,13 @@ func main() {
 	if err != nil {
 		log.Fatalln(err)
 	}
-	//katetA := float64(lenInt)
-
+	
 	fmt.Printf("Введите длину катета (b) прямоугольного треугольника\n")
 	fmt.Scanln(&lenLn)
 	katetB, err := strconv.ParseFloat(lenLn, 64)
 	if err != nil {
 		log.Fatalln(err)
 	}
-	//katetB := float64(lenInt)
 
 	areaTringle := 0.5 * katetA * katetB
 	fmt.Printf("Площадь данного треугольника равняется %.2f\n", areaTringle)
--- homework-1/BankDeposit/main.go
+++ homework-1/BankDeposit/main.go
@@ -7,12 +7,30 @@ import (
 )
 
 func main() {
-	fmt.Printf("Введите сумму в рублях на которую вы хотите открыть вклад?\n")
-	sumLn := "0"
-	fmt.Scan(&sumLn)
-	sumInt, err := strconv.Atoi(lenLn)
+	fmt.Printf("Введите сумму в рублях на которую вы хотите открыть вклад:\n")
+	userInput := "10000"
+	fmt.Scan(&userInput)
+	deposit, err := strconv.ParseFloat(userInput, 64)
 	if err != nil {
 		log.Fatalln(err)
 	}
-	katetA := float64(lenInt)
+
+	fmt.Printf("Введите процент который будет начисляться ежегодно на ваш вклад:\n")
+	fmt.Scan(&userInput)
+	procent, err := strconv.ParseFloat(userInput, 64)
+	if err != nil {
+		log.Fatalln(err)
+	}
+
+	fmt.Printf("Введите колличество лет на которые открывается вклад:\n")
+	fmt.Scan(&userInput)
+	numbeYear, err := strconv.Atoi(userInput)
+	if err != nil {
+		log.Fatalln(err)
+	}
+
+	for kolLet := 1; kolLet <= numbeYear; kolLet++ {
+		deposit += deposit * procent / 100.00
+		fmt.Printf("Через %d год размер вашего вклада составит %.2f рублей.\n", kolLet, deposit)
+	}
 }
