7cbdf01f8781cc4f01cc6965662258c06e3dd3d7
Ivlev Aleksandr <a.y.ivlev@gmail.com>
2020-06-02 20:10:12 +0300

    New fixed realise

--- homework-1/main.go
+++ homework-1/main.go
@@ -3,19 +3,19 @@ package main
 import (
 	"fmt"
 
-	"github.com/GoCourse/homework-1/AreaTringle"
-	"github.com/GoCourse/homework-1/BankDeposit"
-	"github.com/GoCourse/homework-1/CurrencyConv"
+	"github.com/GoCourse/homework-1/areaTringle"
+	"github.com/GoCourse/homework-1/bankDeposit"
+	"github.com/GoCourse/homework-1/currencyConv"
 )
 
 func main() {
 
 	fmt.Println("1. Программа для конвертации рублей в доллары и в евро.\n Программа запрашивает сумму в рублях и выводит сумму в долларах и в евро.\n")
-	CurrencyConv.CurrencyConv()
+	currencyConv.CurrencyConv()
 
 	fmt.Println("\n2. Программа для расчёта площади, периметра и гипотенузы прямоугольного треугольника.\n Пользователь вводит с клавиатуры длину катетов (a) и (b) прямоугольного треугольника.\n")
-	AreaTringle.AreaTringle()
+	areaTringle.AreaTringle()
 
 	fmt.Println("\n3. Программа для расчета суммы вклада и процентов по нему через колличество лет заданных пользователем.\n Программа запрашивает сумму вклада в рублях, процент под который размещается вкалад и колличество лет на которые открывается вклад.\n")
-	BankDeposit.BankDeposit()
+	bankDeposit.BankDeposit()
 }
