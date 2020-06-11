5729bad33cd72f9dba189ae90d4d7f4b318d50d9
origin <a.y.ivlev@gmail.com>
2020-06-11 12:42:59 +0300

    Added calculator

---
+++ homework-4/main.go
@@ -0,0 +1,28 @@
+
+package main
+
+import (
+	"github.com/GoCourse/homework-4/calculator"
+	"fmt"
+)
+
+func main() {
+	input := ""
+	for {
+		fmt.Print("> ")
+		if _, err := fmt.Scanln(&input); err != nil {
+			fmt.Println(err)
+			continue
+		}
+
+		if input == "exit" {
+			break
+		}
+
+		if res, err := calculator.Calculate(input); err == nil {
+			fmt.Printf("Результат: %v\n", res)
+		} else {
+			fmt.Println("Не удалось произвести вычисление")
+		}
+	}
+}
