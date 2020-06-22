7d1704ed56229499fa28bb839eacebc7e03585c4
origin <a.y.ivlev@gmail.com>
2020-06-22 19:21:02 +0300

    Added Time server

---
+++ homework-7/timeServer/server.go
@@ -0,0 +1,42 @@
+package main
+
+import (
+	"fmt"
+	"io"
+	"log"
+	"net"
+	"time"
+)
+
+func main() {
+	listener, err := net.Listen("tcp", "localhost:8000")
+	if err != nil {
+		log.Fatal(err)
+	}
+	for {
+		conn, err := listener.Accept()
+		if err != nil {
+			log.Print(err)
+			continue
+		}
+		go handleConn(conn)
+
+		userInput := ""
+		fmt.Println("Для завершения работы сервера наберите exit")
+		fmt.Scan(&userInput)
+		if userInput == "exit" {
+			break
+		}
+	}
+}
+
+func handleConn(c net.Conn) {
+	defer c.Close()
+	for {
+		_, err := io.WriteString(c, time.Now().Format("15:04:05\n\r"))
+		if err != nil {
+			return
+		}
+		time.Sleep(1 * time.Second)
+	}
+}
